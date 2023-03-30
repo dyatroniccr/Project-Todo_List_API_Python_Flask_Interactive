1ca61bce304706aff3aa3281a42ae542d3a83581
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-30 02:39:52 +0000

    Finish - Update 8.2

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
 todos = [
     { "label": "My first task", "done": False },
-    #{ "label": "My second task", "done": False }
+    { "label": "My second task", "done": False }
 ]
 
 
@@ -12,23 +13,44 @@ todos = [
 def hello_world():
     return 'Hello, World!'
 
-"""
+
 @app.route('/todos', methods=['GET'])
 def todo_get():
     json_text = jsonify(todos)
     return json_text
     #return '<h1>Hello!</h1>'
 
-"""
+
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     #request_body = request.data
-    #request_body = request.get_json()
-    request_body = request.json
+    request_body = request.get_json()
+    #request_body = request.json
+    #decoded_object = json.loads(request_body)
     print("Incoming request with the following body", request_body)
     todos.append(request_body)
+    
     json_text = jsonify(todos)
-    return 'Response for the POST todo'
+    #return 'Response for the POST todo'
+    #json_text = jsonify({"lista":todos, "message":"ok"})
+    return json_text
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    #return 'something'
+    
+    position = int(position)
+    if position >= len(todos):
+        return jsonify({"message":"indice invalido"})
+    if position < 0:
+        return jsonify({"message":"indice invalido"})
+    if len(todos) == 0:
+        return jsonify({"message":"indice invalido"})
+    
+    todos.pop(position)
+    json_text = jsonify(todos)
+    return json_text
 
 #python app <=> __main__
 
