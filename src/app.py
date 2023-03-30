45ffeae3ec18a9d94f6e411df8a907f628b52190
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-30 00:37:55 +0000

    Update

--- src/app.py
+++ src/app.py
@@ -1,2 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
+
+
+
+
+
+
+#python app <=> __main__
+
+
+
+
+
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
